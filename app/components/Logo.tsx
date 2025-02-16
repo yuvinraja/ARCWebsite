51e5a256d2239bdcaac90c1ee5c2b7619a950594
Yuvin Raja <yuvinrajav@gmail.com>
2025-02-16 17:02:46 +0530

    add navbar

---
+++ app/components/Logo.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <>
+        <div className="text-center">
+        <h1 className="text-lg font-bold text-gray-800">VOLTAERO</h1>
+        <p className="text-xs text-gray-600 mt-1">Innovate, Elevate, Accelerate</p>
+    </div>
+    </>
+  )
+}
+
+export default Logo
