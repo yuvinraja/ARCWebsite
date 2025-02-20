e492d6ac5806264d69e74847042910eb1d5ccca2
Yuvin Raja <yuvinrajav@gmail.com>
2025-02-20 23:51:09 +0530

    add footer and fix some issues

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -17,21 +17,25 @@ const Hero = () => {
       </video>
 
       {/* Overlay Content */}
-      <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white bg-black bg-opacity-50">
-        <h1 className="text-5xl font-bold mb-6">Empowering Innovation</h1>
-        <p className="text-lg mb-8">Access Experiments, Build the Future.</p>
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white bg-black bg-opacity-50 px-4">
+        <h1 className="text-4xl md:text-5xl font-bold mb-4 md:mb-6">
+          Empowering Innovation
+        </h1>
+        <p className="text-base md:text-lg mb-6 md:mb-8">
+          Access Experiments, Build the Future.
+        </p>
 
         {/* Buttons */}
-        <div className="flex space-x-4">
+        <div className="flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-4">
           <Link
             href="/experiments"
-            className="px-6 py-3 border border-black-500 hover:border-blue-600 h text-white font-semibold rounded-lg transition hover:text-blue-400 hover:font-bold"
+            className="px-6 py-3 border border-blue-500 hover:border-blue-600 text-white font-semibold rounded-lg transition hover:text-blue-400 hover:font-bold"
           >
             Explore Experiments
           </Link>
           <Link
             href="/projects"
-            className="px-6 py-3 border border-black-500 hover:border-green-600 text-white font-semibold rounded-lg transition hover:text-green-400 hover:font-bold"
+            className="px-6 py-3 border border-green-500 hover:border-green-600 text-white font-semibold rounded-lg transition hover:text-green-400 hover:font-bold"
           >
             Explore Projects
           </Link>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/app/components/Navbar";
+import Footer from "@/app/components/Footer";
 
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
